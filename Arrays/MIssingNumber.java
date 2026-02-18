b8ef9e547ecb092ad45261afd2485d433a3b1ae7
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-02-18 11:12:22 +0530

    Update MIssingNumber.java

--- Arrays/MIssingNumber.java
+++ Arrays/MIssingNumber.java
@@ -1,1 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        // code here
+        long n=nums.length,sum=0;
+        long s=n*(n+1)/2;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        
+        return (int)(s-sum);
+        
+    }
+}
 
