b035213647472b7a8a7c04637277ddc56b8475de
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-02-18 11:44:20 +0530

    Update FindDuplicate.java

--- Arrays/FindDuplicate.java
+++ Arrays/FindDuplicate.java
@@ -1,1 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+    Set<Integer> set=new HashSet<>();
+    for(int num:nums){
+        if(set.contains(num)){
+        return num;//Duplicate number
+        }
+        set.add(num); 
+    }    
+    return -1;
 
+    }
+}
