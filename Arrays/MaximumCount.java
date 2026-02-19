de770954aea510343eb631c2c265fe3349717b6c
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-02-19 23:29:36 +0530

    Add maximumCount method to count positives and negatives
    Implement maximumCount method to count positive and negative integers.

--- Arrays/MaximumCount.java
+++ Arrays/MaximumCount.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int maximumCount(int[] nums) {
+        int negValue=0,posValue=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                posValue++;
+            }else if(nums[i]<0){
+                negValue++;
+            }
+        }
+        return Math.max(negValue,posValue);
+    }
+}
