2d6cee09c41e85a987835a6c2088ffe05fff8618
Navikant22 <142095492+Navikant22@users.noreply.github.com>
2026-02-20 18:06:33 +0530

    Implement nextGreatestLetter method in Solution class

--- Arrays/NextGreatestLetter.java
+++ Arrays/NextGreatestLetter.java
@@ -1,1 +1,13 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int c=(int)target;
+        for(int i=0;i<letters.length;i++){
 
+            if((int)letters[i]>c){
+                return (char)letters[i];
+
+            }
+        }
+         return letters[0];
+    }
+}
